e50f1eb56e429ebd0224fbdb8562e06e896ea9c8
Davis Henckel <51898571+DavisHenckel@users.noreply.github.com>
2021-12-04 13:37:50 -0800

    Add function to Validate Job Title

--- Validators.ps1
+++ Validators.ps1
@@ -34,3 +34,31 @@ Function ValidateLocationCode ($LocationCode) {
     }
     return $LocationCode
 }
+
+#Imports the excel sheet with all the valid Job titles.
+#Takes in a job title and returns a valid job title.
+Function ValidateJobTitle ($JobTitle) {
+    Try {
+        $ExcelFilePath = $PSScriptRoot + "\Excel Dependencies\FileName.xlsm"
+        $AccessFile = Import-Excel $ExcelFilePath -WorksheetName "WorkSheetName"
+    }
+    Catch {
+        Write-Host "ERROR -- Could not read file. Exiting" -ForegroundColor Red
+        Pause
+        exit
+    }
+    $JobTitleArray = $AccessFile."Job Titles"
+    while ($true) { #infinite loop
+        if ($JobTitleArray.Contains($JobTitle)) {
+            break
+        }
+        #Allows an invalid job title to be entered.
+        if ($JobTitle -match " -OVERRIDE") {
+            return $JobTitle
+        }
+        Write-Host "`nERROR -- Job Title `"$JobTitle`" doesn't exist.`nYou can override this validation and continue the script with an unvalidated Job title if you like.`nTo do so, enter the job title followed by `" -OVERRIDE`"`nExample: SomeJobNotDefined -OVERRIDE" -ForegroundColor Red
+        $JobTitle = Read-Host -Prompt "Enter a Job Title"
+        Write-Host "`n"
+    }
+    return $JobTitle
+}
