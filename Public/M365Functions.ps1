d242244dbefd7cdd6af6b09bd670c0a1287791d8
Davis Henckel <51898571+DavisHenckel@users.noreply.github.com>
2021-12-06 22:14:51 -0800

    Function to assign office licenses

--- Public/M365Functions.ps1
+++ Public/M365Functions.ps1
@@ -380,3 +380,75 @@ Function DisplayUsersWithOfficeLicenses ($Enabled) {
     Write-Host "Function completed." -ForegroundColor Green
     return
 }
+
+#Takes in a license name.
+# Returns true if the given Office 365 License is Available and can be assigned
+# Returns false if the given license is not available
+Function IsO365LicenseAvailable ($LicenseName) {
+    ConnectMSOL
+    if ($LicenseName -eq "Exchange Online Kiosk") {
+        $LicenseName = 'wilcocoop:EXCHANGEDESKLESS'
+    }
+    elseif ($LicenseName -eq "E1") {
+        $LicenseName = 'wilcocoop:STANDARDPACK'
+    }
+    elseif ($LicenseName -eq "E3") {
+        $LicenseName = 'wilcocoop:ENTERPRISEPACK'
+    }
+    elseif ($LicenseName -eq "F3") {
+        $LicenseName = 'wilcocoop:DESKLESSPACK'
+    }
+    else { 
+        Write-Host "License `"$Office365LicenseToAdd`" doesn't exist." -ForegroundColor Red
+        return $false
+    }
+
+    $O365License = Get-MsolAccountSku | Where-Object {$_.AccountSkuId -eq $LicenseName}
+    $NumAvailableLicenses = $O365License.ActiveUnits - $O365License.ConsumedUnits
+    If ($NumAvailableLicenses -lt 1) {
+        return $false
+    }
+    return $true
+}
+
+#Takes in a SAMACcountName
+#Takes in an office license to be added
+#Assigns the office license to the user.
+Function AssignOfficeLicense ($sAMAccountName, $Office365LicenseToAdd) {
+    $LicName = $Office365LicenseToAdd
+    $EmailAddress = GetADUserCustom -SearchBy sAMAccountName -SearchFor $sAMAccountName -ReturnData Mail
+    ConnectMSOL
+    $PotentialError1 = $null
+    $PotentialError2 = $null
+    Set-MsolUser -UserPrincipalName $EmailAddress -UsageLocation US -ErrorVariable PotentialError1 -ErrorAction SilentlyContinue
+    Set-MsolUserLicense -UserPrincipalName $EmailAddress -AddLicenses $Office365LicenseToAdd -ErrorVariable PotentialError2 -ErrorAction SilentlyContinue
+    if ($PotentialError1 -ne "" -or $PotentialError2 -ne "") {
+        #Write-Host "ERROR -- Unable to assign $Office365LicenseToAdd to $EmailAddress." -ForegroundColor Red
+        return $false
+    }
+    Write-Host "Added " -ForegroundColor Green -NoNewline
+    Write-Host "$LicName" -ForegroundColor Cyan -NoNewline
+    Write-Host " license successfully!" -ForegroundColor Green 
+    return $true
+}
+
+#Takes in a SAMACcountName
+#Takes in an office license to be added
+#Assigns the office license to the user.
+Function AssignOfficeLicense ($sAMAccountName, $Office365LicenseToAdd) {
+    $LicName = $Office365LicenseToAdd
+    $EmailAddress = GetADUserCustom -SearchBy sAMAccountName -SearchFor $sAMAccountName -ReturnData Mail
+    ConnectMSOL
+    $PotentialError1 = $null
+    $PotentialError2 = $null
+    Set-MsolUser -UserPrincipalName $EmailAddress -UsageLocation US -ErrorVariable PotentialError1 -ErrorAction SilentlyContinue
+    Set-MsolUserLicense -UserPrincipalName $EmailAddress -AddLicenses $Office365LicenseToAdd -ErrorVariable PotentialError2 -ErrorAction SilentlyContinue
+    if ($PotentialError1 -ne "" -or $PotentialError2 -ne "") {
+        #Write-Host "ERROR -- Unable to assign $Office365LicenseToAdd to $EmailAddress." -ForegroundColor Red
+        return $false
+    }
+    Write-Host "Added " -ForegroundColor Green -NoNewline
+    Write-Host "$LicName" -ForegroundColor Cyan -NoNewline
+    Write-Host " license successfully!" -ForegroundColor Green 
+    return $true
+}
